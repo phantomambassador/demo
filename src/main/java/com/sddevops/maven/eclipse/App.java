bb999a19f84ee889f26f919426b78b04316ad89c
AIDIL <aidil_muska@hotmail.com>
2026-06-26 12:24:29 +0800

    feat: Add introductory line to test commit and push feature

--- src/main/java/com/sddevops/maven/eclipse/App.java
+++ src/main/java/com/sddevops/maven/eclipse/App.java
@@ -1,11 +1,9 @@
 package com.sddevops.maven.eclipse;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Ruddy Classmate!");
         System.out.println("change line!");
-    }
+        System.out.println("first change");
+        }
 }
